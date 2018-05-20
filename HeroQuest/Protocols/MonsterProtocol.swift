2bfc59a859676a3fd504178e88331681d47bce33
Michael Vilabrera <m.vilabrera@gmail.com>
2018-05-19 22:23:55 -0400

    add Monster protocol

--- HeroQuest/Protocols/MonsterProtocol.swift
+++ HeroQuest/Protocols/MonsterProtocol.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+protocol Monster {
+    var movement: Int { get set }
+    var attack: Int { get set }
+    var defend: Int { get set }
+    var fullHP: Int { get set }
+    var fullMP: Int { get set }
+    
+    func moveAction() -> Int
+}
